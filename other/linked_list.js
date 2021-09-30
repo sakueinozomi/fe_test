1f0204b6f95b70411e6cf401851e523025cfad7d
Lucille Chiang <lchiang.tw@ffn.com>
2021-09-30 17:14:41 +0800

    done the linked list

--- other/linked_list.js
+++ other/linked_list.js
@@ -1,14 +1,40 @@
 class Stack{
     constructor() {
-        this
+        this.data_length = 0;
+        this.lastNode = null;
     }
-    push(){
-
+    _createNode(val) {
+        const node = {
+            val,
+            nextNode: null,
+            preNode: null
+        }
+        return node;
+    }
+    push(data){
+        const node = this._createNode(data);
+        if (this.lastNode) {
+            node.preNode = this.lastNode;
+            this.lastNode.nextNode = node;
+        }
+        this.data_length++;
+        this.lastNode = node;
     }
     pop(){
-
+        if ( this.data_length === 0 ){
+            return null;
+        }
+        const node = this.lastNode;
+        const last = node.preNode;
+        if (last) {
+            last.next = null;
+        }
+        this.lastNode = last;
+        node.preNode = null; //disconect this poped object's binding.
+        this.data_length--;
+        return node.val;
     }
     size(){
-
+        return this.data_length;
     }
 }
--- other/throttle.js
+++ other/throttle.js
@@ -1,14 +1,12 @@
 var throttle = (func, timeout) => {
     let last_execution;
-    let count_timer;
+    let count_timer; //to record the timer, that can be clear timeout. 
     return function(){
         let now = new Date().getTime();
         if(!last_execution || now > timeout + last_execution) {
-            console.log("go if", last_execution, now)
             last_execution = now;
             func.apply(this, arguments); 
         } else {
-            console.log("go else", last_execution, now)
             clearTimeout(count_timer)
             count_timer = setTimeout(function(){
                 last_execution = now;
