f277dfc376d0f4c854eec1261b63c77dcb0373d5
Lucille Chiang <lchiang.tw@ffn.com>
2021-09-30 12:46:07 +0800

    first version throttle, testing

--- other/throttle.js
+++ other/throttle.js
@@ -1,4 +1,15 @@
 var throttle = (func, timeout) => {
-    let timer;
-    let last;
+    let last_execution;
+    return function(){
+        let now = new Date().getTime();
+        if(!last_execution || now > timeout + last_execution) {
+            last_execution = now;
+            func.apply(this, arguments);
+        } else {
+            setTimeout(function(){
+                last_execution = now;
+                func.apply(this, arguments);
+            },timeout)
+        }
+    }
 }
