e9aa37d7ea5d40acf18dafa5ebf8b24692a60b62
Lucille Chiang <lchiang.tw@ffn.com>
2021-09-30 14:20:52 +0800

    done throttle

--- other/throttle.js
+++ other/throttle.js
@@ -1,12 +1,16 @@
 var throttle = (func, timeout) => {
     let last_execution;
+    let count_timer;
     return function(){
         let now = new Date().getTime();
         if(!last_execution || now > timeout + last_execution) {
+            console.log("go if", last_execution, now)
             last_execution = now;
-            func.apply(this, arguments);
+            func.apply(this, arguments); 
         } else {
-            setTimeout(function(){
+            console.log("go else", last_execution, now)
+            clearTimeout(count_timer)
+            count_timer = setTimeout(function(){
                 last_execution = now;
                 func.apply(this, arguments);
             },timeout)
